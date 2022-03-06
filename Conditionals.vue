61c41ea78e0844d1e6b300d23d8a09f017b41532
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-03-06 10:40:11 +0530

    Filter method added

--- Conditionals.vue
+++ Conditionals.vue
@@ -19,6 +19,12 @@
       <h1>Name is {{ name }}</h1>
       <button @click="changeName('Abhishek')">Change</button>
     </div>
+    <div class="bg-neutral-900 text-slate-200 py-6 text-center text-3xl font-mono">
+      <h1>Original Numbers are {{ numbers }}</h1>
+      <h1>Filter Numbers are {{ filterNumbers }}</h1>
+      <h1>{{error}} </h1>
+      <button @click="filterNumber(numbers)">Change</button>
+    </div>
   </div>
 </template>
 
@@ -28,21 +34,32 @@ export default {
   data() {
     return {
       name: "Bharat",
-      display:true,
-
+      display: true,
+      numbers: [1, 3, 5, 7, 9, 11, 12, 13, 14, 15, 16, 17, 18],
+      filterNumbers: [],
+      error:""
     };
   },
   methods: {
     changeName(name) {
-      if(this.name == name)
-      {
+      if (this.name == name) {
         this.name = "Shubham";
-      }
-      else{
+      } else {
         this.name = name;
       }
-    }
-  }
+    },
+    filterNumber(array) {
+      let userInput = parseInt(prompt("Enter a number"));
+      let filtered = array.filter((item) => item > userInput);
+      if (this.filterNumbers.length >= 0 && !isNaN(userInput)) {
+        this.filterNumbers = filtered;
+        this.error = ""
+      }
+      else{
+        this.error = "Please enter a number to filter the array"
+      }
+    },
+  },
 };
 </script>
 
