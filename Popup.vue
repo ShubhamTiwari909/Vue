2d146a3448809b95dad0700878d19dd653ce0566
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-03-08 18:27:30 +0530

    Popup and ProvideInject files has been added

---
+++ Popup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-slate-800 py-5 my-10">
+      <h1 class="text-center text-slate-200 py-5 text-2xl my-5">This is a Popup</h1>
+      <button class="bg-gradient-to-r from-red-200 via-red-300 to-red-400 px-5 py-2 rounded-md"
+       @click="$emit('close')">Close Popup</button>
+  </div>
+</template>
+
+<script>
+export default {
+name:"Popup",
+emits:["close"]
+}
+</script>
+
+<style>
+
+</style>
---
+++ ProvideInject.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="bg-gray-800 text-red-300 text-center text-3xl font-serif py-10">
+      Provide and Inject method is used to pass down the data to the root level
+      easily - <p class="text-indigo-400">{{ username }}</p>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProvideInject",
+  inject: ["username"],
+};
+</script>
+
+<style></style>
