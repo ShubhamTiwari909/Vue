2d13828c127893604ddf35ffc84037bc62dd80f9
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-03-12 13:31:22 +0530

    CompVModel file has been added

---
+++ CompVModel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-stone-900 py-10 text-center">
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="ring-2 ring-indigo-400 rounded px-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+    />
+
+    <h1 class="my-5 text-slate-200">{{modelValue}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CompVModel",
+  props: {
+    modelValue: String,
+  },
+};
+</script>
+
+<style></style>
