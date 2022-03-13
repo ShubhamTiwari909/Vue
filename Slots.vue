b3f69c51edae79af4f5eee992b3e5fa4c3e0e3e2
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-03-13 12:27:15 +0530

    Slots file has been added

---
+++ Slots.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <!-- Normal slots -->
+      <!-- <div>
+        <slot>This is the Default Value of slot tag</slot>
+      </div> -->
+      <!-- Named slots -->
+      <div>
+          <div>
+              <slot name="Header"></slot>
+          </div>
+          <div>
+              <slot name="Content"></slot>
+          </div>
+          <div>
+              <slot name="Footer"></slot>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
