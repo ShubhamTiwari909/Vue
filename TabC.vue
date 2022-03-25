36875eb617327c151ecc80036f85babcca9f0240
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-03-25 12:43:28 +0530

    Minor changes in TabControl file

--- TabC.vue
+++ TabC.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="text-purple-600 my-10 text-xl">
-      This is TAB C
+      <h1>This is TAB C</h1>
+      <input type="text" v-model="name" class="ring-2 ring-purple-400 rounded-md px-2.5 py-2 my-10">
   </div>
 </template>
 
 <script>
 export default {
-name:"TabC"
+name:"TabC",
+data() {
+  return{
+    name:""
+  }
+}
 }
 </script>
 
--- TabControl.vue
+++ TabControl.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="bg-neutral-800 text-slate-300 py-20 text-center">
-    <button v-bind:class="classButton + ' text-cyan-300 border-2 border-cyan-300'" @click="activeTab = 'TabA'">TabA</button>
-    <button v-bind:class="classButton + ' text-indigo-300 border-2 border-indigo-300'" @click="activeTab = 'TabB'">TabB</button>
-    <button v-bind:class="classButton + ' text-purple-500 border-2 border-purple-500'" @click="activeTab = 'TabC'">TabC</button>
+    <button
+      v-bind:class="classButton + ' text-cyan-300 border-2 border-cyan-300'"
+      @click="activeTab = 'TabA'"
+    >
+      TabA
+    </button>
+    <button
+      v-bind:class="classButton + ' text-indigo-300 border-2 border-indigo-300'"
+      @click="activeTab = 'TabB'"
+    >
+      TabB
+    </button>
+    <button
+      v-bind:class="classButton + ' text-purple-500 border-2 border-purple-500'"
+      @click="activeTab = 'TabC'"
+    >
+      TabC
+    </button>
 
     <!-- FIRST METHOD USING v-if -->
     <!-- <div>
@@ -11,29 +26,31 @@
         <TabC v-if="activeTab === 'TabC'" />
     </div> -->
 
-    <!-- SECOND METHOD USING component tag-->
-    <component :is="activeTab" />
+    <!-- SECOND METHOD USING component tag with keep-alive tag which is used to save the state of the tab and won't reset the value if the tab changes to other tabs -->
+    <keep-alive>
+      <component :is="activeTab" />
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TabA from './TabA.vue'
-import TabB from './TabB.vue'
-import TabC from './TabC.vue'
+import TabA from "./TabA.vue";
+import TabB from "./TabB.vue";
+import TabC from "./TabC.vue";
 export default {
-name:"TabControl",
-components:{
-    TabA,TabB,TabC
-},
-data() {
+  name: "TabControl",
+  components: {
+    TabA,
+    TabB,
+    TabC,
+  },
+  data() {
     return {
-        activeTab : "TabA",
-        classButton:"rounded-md px-5 py-2 mx-10"
-    }
-}
-}
+      activeTab: "TabA",
+      classButton: "rounded-md px-5 py-2 mx-10",
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
